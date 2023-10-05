de1ae3fbed8ed9db77f56ab8226b583147cba443
Éverton Carvalho <evertonsnake@gmail.com>
2023-10-05 09:37:58 -0300

    StockItems

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { StockContextProvider } from "./contexts/StockContext";
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
 export default function App() {
   return (
-    <>
+    <StockContextProvider>
       <RouterProvider router={router} />
-    </>
+    </StockContextProvider>
   );
 }
